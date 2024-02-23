83625fc2a606ae019d7387c5b0ed301d8359e654
AlfinFauzan <160990567+AlfinFauzan@users.noreply.github.com>
2024-02-23 10:32:40 +0700

    Add files via upload

---
+++ like.php
@@ -0,0 +1,26 @@
+<?php
+	include 'koneksi.php';
+	session_start();
+	
+	if(!isset($_SESSION['UserID'])) {
+		// untuk like harus login dulu
+		echo '<script>alert("Login terlebih"); documen.location="Login.php";</script>';
+	}else{
+		$FotoID=$_GET['FotoID'];
+		$UserID=$_SESSION['UserID'];
+		// cek apakah user sudah pernah like foto
+		
+		$sql=mysqli_query($koneksi, "select *  from likefoto where FotoID='$FotoID' and UserID='$UserID'");
+		
+		if(mysqli_num_rows($sql)==1){
+		// user sudah pernah like foto ini
+		echo '<script>alert("Anda sudah menyukai foto ini" document.location="index.php";</script>';
+		}else{
+			$tanggalike=date('Y-m-d');
+			mysqli_query($koneksi, "insert into likefoto values('','$FotoID', '$UserID', '
+			$TanggaLike')");
+			echo '<script>alert("terimakasih") ;document.location="index.php";</script>';
+		}
+	}
+?>
+		
